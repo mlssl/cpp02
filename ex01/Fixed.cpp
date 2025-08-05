f7db20e94e9221f22466e708f1ef286f15008914
Mathilde Laussel <mlaussel@z4r2p6.42lyon.fr>
2025-08-05 14:29:10 +0200

    start ex01, add functions and constructors, needs to implement them

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlaussel <mlaussel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 12:21:38 by mlaussel          #+#    #+#             */
+/*   Updated: 2025/08/05 14:24:09 by mlaussel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int Fixed::_bit = 8;
+
+											/*CANONIC PART*/
+
+//constructor
+Fixed::Fixed() : _n(0)
+{
+	std::cout << GREEN << "Default constructor called" << STOP << std::endl;
+}
+
+//destructor
+Fixed::~Fixed()
+{
+	std::cout << RED << "Destructor called" << STOP << std::endl;
+}
+
+//copy
+Fixed::Fixed(Fixed const &f)
+{
+	std::cout << GREEN << "Copy constructor called" << STOP << std::endl;
+	*this = f;
+}
+
+//operator =
+Fixed &Fixed::operator=(Fixed const & rs)
+{
+	std::cout << GREEN << "Copy assignment operator called" << STOP << std::endl;
+	this->_n = rs.getRawBits();
+
+	return (*this);
+}
+
+
+										/*CONSTRUCTORS*/
+
+//int
+Fixed::Fixed(const int intValue)
+{
+	std::cout << GREEN << "Int constructor called" << STOP << std::endl;
+}
+
+Fixed::Fixed(const float floatValue)
+{
+	std::cout << GREEN << "Float constructor called" << STOP << std::endl;
+}
+
+
+											/*OPERATOR*/
+//operator <<
+std::ostream & operator<<(std::ostream & o, Fixed const & rs)
+{
+	o << rs.toFloat();
+	return (o);
+}
+
+
+										/*GETTER AND SETTER*/
+//getters
+int	Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_n);
+}
+
+//setters
+void	Fixed::setRawBits(int const raw)
+{
+	this->_n = raw;
+}
+
+
+									/*MEMBER FUNCTION*/
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlaussel <mlaussel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 11:32:32 by mlaussel          #+#    #+#             */
+/*   Updated: 2025/08/05 14:16:22 by mlaussel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE  "\033[34m"
+#define STOP "\033[0m"
+
+class Fixed
+{
+	public :
+		Fixed(); //constructor
+		Fixed(Fixed const & f); //copy
+		~Fixed(); //destructor
+		Fixed & operator=(Fixed const & rs); //operator
+
+		Fixed(const int intValue);
+		Fixed(const float floatValue);
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+
+		float	toFloat(void) const;
+		int		toInt( void ) const;
+
+	private :
+		int _n;
+		static const int _bit;
+};
+
+std::ostream & operator<<(std::ostream & o, Fixed const & rs);
+
+#endif
